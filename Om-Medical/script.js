071ff4f1bed131c3165bd153ae0fc46785108f55
Pavan <pavan561981@gmail.com>
2026-01-29 13:01:57 +0530

    landing page

---
+++ Om-Medical/script.js
@@ -0,0 +1,12 @@
+function toggleMenu() {
+  document.getElementById("navLinks").classList.toggle("active");
+}
+
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+  link.addEventListener("click", e => {
+    e.preventDefault();
+    document.querySelector(link.getAttribute("href"))
+      .scrollIntoView({ behavior: "smooth" });
+    document.getElementById("navLinks").classList.remove("active");
+  });
+});
