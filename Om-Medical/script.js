cf8f06cd62f1af5ad61588a3da17e1ffaa56aa90
Pavan <pavan561981@gmail.com>
2026-01-29 18:43:09 +0530

    Script updated

--- Om-Medical/script.js
+++ Om-Medical/script.js
@@ -1,3 +1,18 @@
+// script js
+function toggleMenu() {
+  document.getElementById("navLinks").classList.toggle("active");
+}
+
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+  link.addEventListener("click", e => {
+    e.preventDefault();
+    document.querySelector(link.getAttribute("href"))
+      .scrollIntoView({ behavior: "smooth" });
+    document.getElementById("navLinks").classList.remove("active");
+  });
+});
+
+
 document.addEventListener("DOMContentLoaded", () => {
   const chatBtn = document.getElementById("chatBtn");
   const chatBox = document.getElementById("chatBox");
